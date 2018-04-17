e37f2cbae626ab062ccea90b14479209a0ae584d
Your Name <278469969@qq.com>
2018-04-17 14:45:06 +0800

    上传了swiper功能，效果暂未实现

--- src/page/home/HomePage.vue
+++ src/page/home/HomePage.vue
@@ -1,11 +1,35 @@
 <template>
  <div>
-    123
+   <header>
+       <p>首页</p>
+   </header>
+
+   <div class="swiper-container">
+    <div class="swiper-wrapper">
+      <div class="swiper-slide">Slide 1</div>
+      <div class="swiper-slide">Slide 2</div>
+      <div class="swiper-slide">Slide 3</div>
+      <div class="swiper-slide">Slide 4</div>
+      <div class="swiper-slide">Slide 5</div>
+      <div class="swiper-slide">Slide 6</div>
+      <div class="swiper-slide">Slide 7</div>
+      <div class="swiper-slide">Slide 8</div>
+      <div class="swiper-slide">Slide 9</div>
+      <div class="swiper-slide">Slide 10</div>
+    </div>
+    <!-- Add Pagination -->
+    <div class="swiper-pagination"></div>
+    <!-- Add Arrows -->
+    <div class="swiper-button-next"></div>
+    <div class="swiper-button-prev"></div>
+  </div>
  </div>
 </template>
 
 <script src="./home-page.js"></script>
+    
 
 <style lang='scss' scoped>
     @import './home-page.scss';
+
 </style>
