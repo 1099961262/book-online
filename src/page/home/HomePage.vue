c1db7f824326e3eab7d46db1a736eb7b1970333f
Your Name <278469969@qq.com>
2018-04-18 13:36:58 +0800

    完善主页面

--- src/page/home/HomePage.vue
+++ src/page/home/HomePage.vue
@@ -1,33 +1,44 @@
 <template>
- <div class="home-page">
-   <header>
-       <p>首页</p>
-   </header>
-
-   <div class="swiper-container">
-    <div class="swiper-wrapper">
-      <div class="swiper-slide banner">
-        <img src="/static/img/1.png">
+  <div class="home-page">
+    <header>
+      <p>首页</p>
+    </header>
+    <!-- 轮播图  -->
+    <div class="swiper-container">
+      <div class="swiper-wrapper">
+        <div class="swiper-slide banner">
+          <img src="/static/img/1.png">
+        </div>
+        <div class="swiper-slide banner">
+          <img src="/static/img/2.png">
+        </div>
+        <div class="swiper-slide banner">
+          <img src="/static/img/3.png">
+        </div>
       </div>
-      <div class="swiper-slide banner">
-        <img src="/static/img/2.png">
-        </div>
-      <div class="swiper-slide banner">
-        <img src="/static/img/3.png">
-        </div>
+      <!-- Add Pagination -->
+      <div class="swiper-pagination"></div>
+      <!-- Add Arrows -->
+      <div class="swiper-button-next"></div>
+      <div class="swiper-button-prev"></div>
     </div>
-    <!-- Add Pagination -->
-    <div class="swiper-pagination"></div>
-    <!-- Add Arrows -->
-    <div class="swiper-button-next"></div>
-    <div class="swiper-button-prev"></div>
+    <!-- 新闻 -->
+    <div class="news">
+      <i class="fa fa-bell"></i>
+      <span>双12全场图书打1折,更有隐藏的神秘惊喜</span>
+    </div>
+    <!-- 内容 -->
+    <div>
+      <HomeRecom title="编辑推荐" :book-info="editBook"/>
+      <HomeRecom title="热门推荐" :book-info="hotBook"/>
+      <HomeRecom title="新书推荐" :book-info="newBook"/>
+    </div>
   </div>
- </div>
 </template>
 
 <script src="./home-page.js"></script>
-    
+
 
 <style lang='scss'>
-    @import './home-page.scss';
+  @import './home-page.scss';
 </style>
